5c08f0cb61240e7a608b2481b757a8a9346ba850
mpiwosal <mpiwosal@gmail.com>
2019-01-28 14:37:56 +0100

    Initial Commit

---
+++ futureMind_recruitmentTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  futureMind_recruitmentTask
+//
+//  Created by Marcin Piwoński on 28/01/2019.
+//  Copyright © 2019 Marcin Piwoński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
